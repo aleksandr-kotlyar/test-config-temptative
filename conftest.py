714e8c2df497d16f8e0420936f420b1594a92833
Andrea Bisello <andrea.bi@2csolution.it>
2020-03-25 15:24:04 +0100

    use case for bug report

--- conftest.py
+++ conftest.py
@@ -1,9 +1,2 @@
-import pytest
-from pytest_testconfig import config
-value = config['something']['a_value']
-
-@pytest.fixture()
-def fixture_me():
-    return value
-
+from lib import do_something
 
