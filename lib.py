4d3e05950800fb6581daa9949ba4ced7886aeac6
aleksandr-kotlyar <ask.kotlyar@gmail.com>
2020-11-02 00:28:30 +0300

    Removed constant initialization with plugin config parser; moved config parser to 'do_something' method; added test_something2 to test 'do_something' method worked. Resolved #8.

--- lib.py
+++ lib.py
@@ -1,6 +1,5 @@
 from pytest_testconfig import config
 
-valore = config['something']['a_value']
 
 def do_something():
-    return valore
+    return config['something']['a_value']
--- test_something.py
+++ test_something.py
@@ -1,5 +1,12 @@
 import pytest
 from pytest_testconfig import config
 
+from lib import do_something
+
+
 def test_something():
     assert config['something']['a_value'] == "the value"
+
+
+def test_something2():
+    assert do_something() == "the value"
